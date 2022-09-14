90b1542894207b26ee075714ef3230912869e009
Joshua Plicque <plicjo@gmail.com>
2022-09-14 09:32:30 -0400

    WIP S3 uploads

--- lib/liveview_mastery_web/live/puppy_live/form_component.ex
+++ lib/liveview_mastery_web/live/puppy_live/form_component.ex
@@ -7,11 +7,36 @@ defmodule LiveviewMasteryWeb.PuppyLive.FormComponent do
   def update(%{puppy: puppy} = assigns, socket) do
     changeset = Puppies.change_puppy(puppy)
 
+
+    socket |> allow_upload(:photo, accept: ~w(.png .jpeg .jpg), max_entries: 1, auto_upload: true, external: &presign_entry/2) |> ok()
+
+
     {:ok,
      socket
      |> assign(assigns)
      |> assign(:changeset, changeset)}
   end
+
+  defp presign_entry(entry, %{assigns: %{uploads: uploads}} = socket) do
+    s3_filepath = SimpleS3Upload.s3_filepath(entry)
+    {:ok, fields} =
+      SimpleS3Upload.sign_form_upload(
+        key: s3_filepath,
+        content_type: entry.client_type,
+        max_file_size: uploads.photo.max_file_size,
+        expires_in: :timer.hours(1)
+      )
+    meta =
+      %{
+        uploader: "S3",
+        key: s3_filepath,
+        url: "https://#{SimpleS3Upload.bucket()}.s3.amazonaws.com",
+        fields: fields
+      }
+
+    {:ok, meta, socket}
+  end
+
 
   @impl true
   def handle_event("validate", %{"puppy" => puppy_params}, socket) do
@@ -24,6 +49,7 @@ defmodule LiveviewMasteryWeb.PuppyLive.FormComponent do
   end
 
   def handle_event("save", %{"puppy" => puppy_params}, socket) do
+    puppy_params = put_photo_urls(socket, puppy_params)
     save_puppy(socket, socket.assigns.action, puppy_params)
   end
 
@@ -52,4 +78,15 @@ defmodule LiveviewMasteryWeb.PuppyLive.FormComponent do
         {:noreply, assign(socket, changeset: changeset)}
     end
   end
+
+  defp put_photo_urls(socket, puppy) do
+    {completed, []} = uploaded_entries(socket, :photo)
+    urls =
+      for entry <- completed do
+        SimpleS3Upload.entry_url(entry)
+      end
+
+    %{puppy | "photo_url" => add_photo_url_to_params(List.first(urls), puppy["photo_url"])}
+  end
+
 end
