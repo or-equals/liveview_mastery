a8add07dafce841bb8771611f9a1d1999e9fd4f9
Joshua Plicque <plicjo@gmail.com>
2023-01-16 11:05:08 -0500

    Upgrade to LiveView 0.18

--- lib/liveview_mastery_web/live/puppy_live/form_component.ex
+++ lib/liveview_mastery_web/live/puppy_live/form_component.ex
@@ -33,7 +33,7 @@ defmodule LiveviewMasteryWeb.PuppyLive.FormComponent do
         {:noreply,
          socket
          |> put_flash(:info, "Puppy updated successfully")
-         |> push_redirect(to: socket.assigns.return_to)}
+         |> push_navigate(to: socket.assigns.return_to)}
 
       {:error, %Ecto.Changeset{} = changeset} ->
         {:noreply, assign(socket, :changeset, changeset)}
@@ -46,7 +46,7 @@ defmodule LiveviewMasteryWeb.PuppyLive.FormComponent do
         {:noreply,
          socket
          |> put_flash(:info, "Puppy created successfully")
-         |> push_redirect(to: socket.assigns.return_to)}
+         |> push_navigate(to: socket.assigns.return_to)}
 
       {:error, %Ecto.Changeset{} = changeset} ->
         {:noreply, assign(socket, changeset: changeset)}
