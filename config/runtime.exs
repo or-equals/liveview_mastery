cc18c8196b2f96bf899f75518005d46d99841a22
Joshua Plicque <plicjo@gmail.com>
2023-11-18 15:45:41 -0500

    Deploying to digital ocean (#11)
    * Add major tips
    
    * Add default Dockerfile
    
    * Configure deployment for Digital Ocean
    
    * Extend timeout
    
    * Update tips
    
    * Add all the other files
    
    * Add rest of configuration

--- config/runtime.exs
+++ config/runtime.exs
@@ -31,10 +31,13 @@ if config_env() == :prod do
   maybe_ipv6 = if System.get_env("ECTO_IPV6"), do: [:inet6], else: []
 
   config :liveview_mastery, LiveviewMastery.Repo,
-    # ssl: true,
+    ssl: true,
+    ssl_opts: [verify: :verify_none],
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-    socket_options: maybe_ipv6
+    socket_options: maybe_ipv6,
+    queue_target: 5000,
+    queue_interval: 5000
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
   # A default value is used in config/dev.exs and config/test.exs but you
