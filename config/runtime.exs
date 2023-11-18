69a61a6ed25cab8672f4db0de121391287c07d68
Joshua Plicque <plicjo@gmail.com>
2023-11-18 13:34:03 -0500

    Deploying to digital ocean (#10)
    * Dockerize application
    
    * Configure for Digital Ocean
    
    * Compile JS assets in Dockerfile
    
    * Add start command

--- config/runtime.exs
+++ config/runtime.exs
@@ -31,7 +31,8 @@ if config_env() == :prod do
   maybe_ipv6 = if System.get_env("ECTO_IPV6"), do: [:inet6], else: []
 
   config :liveview_mastery, LiveviewMastery.Repo,
-    # ssl: true,
+    ssl: true,
+    ssl_opts: [verify: :verify_none],
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
     socket_options: maybe_ipv6
